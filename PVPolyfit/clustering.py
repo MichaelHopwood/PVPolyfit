a7505fe0a12843e9b0d1d8bcf7872ea40348cc2b
ADAM SAWICKI <sawicki.adam.j@gmail.com>
2019-11-13 00:18:12 -0500

    Python-ified.  See message.
    I implemented some Python best practices, namely:
    1) Replace all
       'for j in len(range(some_iterable))'
       constructs with a
       'for index, element in enumerate(some_iterable)'
       construct.
       This is more efficient, preferred Python, and
       improves readability by reducing code clutter.
    2) Use built in pd functions (such as '.sum()', '.max()', etc)
       as opposed to multiple np castings followed by an
       aggregation function.

--- PVPolyfit/clustering.py
+++ PVPolyfit/clustering.py
@@ -8,31 +8,25 @@ import pandas as pd
 # Source
 from PVPolyfit import kernel
 from sklearn.cluster import KMeans
- 
+
 
 def classify_weather_day_MHopwood(cut_results, Y_tag, xs, kmeans_num_clusters=4):
     # make parameters for sub-daily
-    kmeans_num_clusters: int = 4
-    classifications: list = []
+    classifications = []
     for df in cut_results:
-        np.array(df.index.tolist())
-        daysecs: list = [
-            (
-                datetime.datetime.strptime(ind, "%m/%d/%Y %H:%M:%S %p")
-                - datetime.datetime.strptime(ind, "%m/%d/%Y %H:%M:%S %p").replace(
-                    hour=0, minute=0, second=0, microsecond=0
-                )
-            ).total_seconds()
-            for ind, row in df.iterrows()
-        ]
+        daysecs = []
+        for index in df.index:
+            # Calculate once
+            _datetime = datetime.datetime.strptime(index, "%m/%d/%Y %H:%M:%S %p")
+            _daysecs = (
+                _datetime
+                - _datetime.replace(hour=0, minute=0, second=0, microsecond=0).total_seconds()
+            )
+            daysecs.append(_daysecs)
 
-        tup_list: list = []
-        for j in range(len(xs)):
-            tup_list.append(np.array(df[xs[j]].tolist()))
+        vals_for_vstack = [np.array(df[x]) for x in xs]
 
-        tup: tuple = tuple(tup_list)
-
-        ordered_pairs: np.ndarray = np.hstack((np.vstack(daysecs), np.vstack(tup).T))
+        ordered_pairs = np.hstack((np.vstack(daysecs), np.vstack(vals_for_vstack).T))
 
         kmeans_day = KMeans(n_clusters=kmeans_num_clusters)
         kmeans_day.fit(ordered_pairs)
@@ -67,34 +61,33 @@ def cluster_ordered_pairs_and_return_df_of_days_in_cluster(
     test_km_labels = km_labels[len(ordered_pair_list) :]
 
     # For cut_results, make new column with each cluster the day belongs to
-    for i in range(len(cut_results)):
-        cut_results[i]["model_num"] = train_km_labels[i]
+    for cut_result, train_km_label in zip(cut_results, train_km_labels):
+        cut_results["model_num"] = train_km_label
 
-    for i in range(len(test_cut_results)):
-        test_cut_results[i]["model_num"] = test_km_labels[i]
+    for test_cut_result, test_km_label in zip(test_cut_results, test_km_labels):
+        test_cut_result["model_num"] = test_km_label
 
     train_kmeans_dfs = [pd.DataFrame() for i in range(kmeans_num_clusters)]
     train_model_day_count = [0] * kmeans_num_clusters
-    for i in range(len(cut_results)):
-        train_model_day_count[train_km_labels[i]] += 1
-        train_kmeans_dfs[train_km_labels[i]] = pd.concat(
-            [train_kmeans_dfs[train_km_labels[i]], cut_results[i]]
+    for index, cut_result in enumerate(cut_results):
+        train_model_day_count[train_km_labels[index]] += 1
+        train_kmeans_dfs[train_km_labels[index]] = pd.concat(
+            [train_kmeans_dfs[train_km_labels[index]], cut_result]
         )
 
     if print_info:
         print("[TRAIN]: NUM DAYS PER MODEL", train_model_day_count)
 
-    test_kmeans_dfs = [pd.DataFrame() for i in range(kmeans_num_clusters)]
+    test_kmeans_dfs = [pd.DataFrame() for _ in range(kmeans_num_clusters)]
     test_model_day_count = [0] * kmeans_num_clusters
-    for i in range(len(test_cut_results)):
-        test_model_day_count[test_km_labels[i]] += 1
-        test_kmeans_dfs[test_km_labels[i]] = pd.concat(
-            [test_kmeans_dfs[test_km_labels[i]], test_cut_results[i]]
+    for index, test_cut_result in enumerate(test_cut_results):
+        test_model_day_count[test_km_labels[index]] += 1
+        test_kmeans_dfs[test_km_labels[index]] = pd.concat(
+            [test_kmeans_dfs[test_km_labels[index]], test_cut_result]
         )
 
     if print_info:
         print("[TEST]: NUM DAYS PER MODEL", test_model_day_count)
-    # print("LENGTH list of dfs: ", len(test_kmeans_dfs), len(test_km_labels))
 
     return (
         train_kmeans_dfs,
@@ -113,22 +106,18 @@ def save_model_for_each_cluster(kmeans_dfs, degree, Y_tag, xs, kernel_type):
     # save a model for each cluster of days
     saved_models = []
     # print"start of model")
-    for i in range(len(kmeans_dfs)):
-        # print"len(kmeans_dfs) for model: ", i, " is ", len(kmeans_dfs[i]))
-        if len(kmeans_dfs[i]) == 0:
+    for kmeans_df in kmeans_dfs:
+
+        if not kmeans_df:
             # if model has no data
             saved_models.append(0)
             continue
 
-        Y = np.array(kmeans_dfs[i][Y_tag].tolist())
-        # Y_log = np.log(Y)
+        Y = np.array(kmeans_df[Y_tag])
 
-        tup_list = []
-        for j in range(len(xs)):
-            tup_list.append(np.array(kmeans_dfs[i][xs[j]].tolist()))
+        inputs = [np.array(kmeans_df[x]) for x in xs]
 
-        tup = tuple(tup_list)
-        model = kernel.Model(tup, Y, degree, kernel_type)
+        model = kernel.Model(inputs, Y, degree, kernel_type)
         model.build()
         saved_models.append(model)
 
@@ -141,17 +130,16 @@ def create_conglomerated_vectors_for_clustering_algorithm(
     normal = "Maybe"
     # normal = True
 
-    if normal == True:
-        freq_list = []
+    big_df = pd.DataFrame()
+
+    if normal == True:  # noqa: ignore
         # ordered pairs used for kmeans
         ordered_pair_list = []
         big_df = pd.DataFrame(columns=["variable", "cloudy", "slightly cloudy", "clear", "hours"])
-        dict_list = []
-        for l in range(len(cut_results)):
-            df = cut_results[l]
-
+        df_rows = []
+        for index, df in enumerate(cut_results):
             # possible other kpi
-            max_v = max(df[Y_tag].tolist())
+            max_v = df[Y_tag].max()
 
             onez, twos, threes, fours = (
                 len(df[df["day_type"] == 1]),
@@ -160,15 +148,8 @@ def create_conglomerated_vectors_for_clustering_algorithm(
                 len(df[df["day_type"] == 4]),
             )
 
-            # consider adding index here, or the time of day
-            freq_df = pd.DataFrame(
-                {"variable": onez, "cloudy": twos, "slightly cloudy": threes, "clear": fours},
-                index=day_hour_list,
-            )
-            freq_list.append(freq_df)
-
             ordered_pair = [
-                hours_kpi[l],
+                hours_kpi[index],
                 np.array(df[Y_tag].tolist()).sum(),
                 max_v,
                 onez,
@@ -178,86 +159,44 @@ def create_conglomerated_vectors_for_clustering_algorithm(
             ]
             ordered_pair_list.append(ordered_pair)
 
-            d = {
+            df_row = {
                 "variable": onez,
                 "cloudy": twos,
                 "slightly cloudy": threes,
                 "clear": fours,
-                "hours": hours_kpi[l],
+                "hours": hours_kpi[index],
                 "mean_power": max_v,
-                "energy": np.array(df[Y_tag].tolist()).sum(),
+                "energy": df[Y_tag].sum(),
             }
-            dict_list.append(d)
+            df_rows.append(df_row)
 
         day_list = list(set(day_hour_list))
 
-        big_df = pd.DataFrame(dict_list, index=day_list)
+        big_df = pd.DataFrame(df_rows, index=day_list)
 
     elif normal == "Maybe":
         # ordered pairs used for kmeans
-        ordered_pair_list = []
-
-        dict_list = []
-        for l in range(len(cut_results)):
-            df = cut_results[l]
-
-            # max Y
-            max_Y = max(df[Y_tag].tolist())
-
-            # total distance Y
-            total_distance_Y = 0
-            for j in range(1, len(cut_results[l])):
-                total_distance_Y += cut_results[l][Y_tag][j] - cut_results[l][Y_tag][j - 1]
-
-            ordered_pair = [hours_kpi[l], max_Y, total_distance_Y]
-            ordered_pair_list.append(ordered_pair)
-
-        big_df = pd.DataFrame()
-
+        ordered_pair_list = [
+            [
+                hours_kpi[index],
+                df[Y_tag].max(),
+                sum((df[Y_tag][j] - df[Y_tag][j - 1] for j in range(1, len(df)))),
+            ]
+            for index, df in enumerate(cut_results)
+        ]
     elif normal == "New":
-
-        # ordered pairs used for kmeans
-        ordered_pair_list = []
-
-        dict_list = []
-        for l in range(len(cut_results)):
-            df = cut_results[l]
-
-            onez, twos, threes, fours = (
-                len(df[df["day_type"] == 0]),
-                len(df[df["day_type"] == 1]),
-                len(df[df["day_type"] == 2]),
-                len(df[df["day_type"] == 3]),
-            )
-
-            # max Y
-            max_Y = max(df[Y_tag].tolist())
-
-            # total distance Y
-            total_distance_Y = 0
-            for j in range(1, len(cut_results[l])):
-                total_distance_Y += cut_results[l][Y_tag][j] + cut_results[l][Y_tag][j - 1]
-
-            # np.array(df[Y_tag].tolist()).sum()]#[onez, twos, threes, fours,
-            # hours_kpi[l], np.array(df[Y_tag].tolist()).sum()]
-            ordered_pair = [hours_kpi[l], max_Y, total_distance_Y]
-            ordered_pair_list.append(ordered_pair)
-
+        ordered_pair_list = [
+            [
+                hours_kpi[index],
+                df[Y_tag].max(),
+                sum((df[Y_tag][j] + df[Y_tag][j - 1] for j in range(1, len(df)))),
+            ]
+            for index, df in enumerate(cut_results)
+        ]
     else:
         # ordered pairs used for kmeans
-        ordered_pair_list = []
-        # big_df = pd.DataFrame(columns = ['variable', 'cloudy', 'slightly cloudy', 'clear', 'hours'])
-        dict_list = []
-        for l in range(len(cut_results)):
-            df = cut_results[l]
-
-            # possible other kpi
-            max_v = np.mean(df[Y_tag].tolist())
-
-            # [onez, twos, threes, fours, hours_kpi[l], np.array(df[Y_tag].tolist()).sum()]
-            ordered_pair = [hours_kpi[l], np.array(df[Y_tag].tolist()).sum()]
-            ordered_pair_list.append(ordered_pair)
-
-        big_df = pd.DataFrame()
+        ordered_pair_list = [
+            [hours_kpi[index], df[Y_tag].sum()] for index, df in enumerate(cut_results)
+        ]
 
     return ordered_pair_list, big_df
