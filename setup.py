b2a99f5f8a5eb5866ddaf6e2205763394332a6ae
Adam Sawicki <adam.sawicki@ibm.com>
2020-01-12 23:25:21 -0500

    Add support for Python 3.7 + 3.8

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 setup(
   name = 'PVPolyfit',
   packages = ['PVPolyfit'],
@@ -27,5 +28,7 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
